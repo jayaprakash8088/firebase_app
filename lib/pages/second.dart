aaf6120eff4c05437b75472c2ae30b14306ac958
Jayaprakash S <70194564+jayaprakash8088@users.noreply.github.com>
2023-10-23 13:51:31 +0530

    new screen added

---
+++ lib/pages/second.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SeconScreen extends StatelessWidget {
+  const SeconScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
